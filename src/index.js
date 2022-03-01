8c82e236262bf1b3e5ea5e1c88be5f170fff7403
Mihai N <74415486+devneagu@users.noreply.github.com>
2022-03-01 14:57:56 +0200

    motion on child

--- src/index.js
+++ src/index.js
@@ -1,40 +1,57 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import SortableTree from "react-sortable-tree";
 import "./index.scss";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
+const index = 3;
 const ReactSortableTreeStyle = {
-  rowHeight: 32, //24
-  scaffoldBlockPxWidth: 24 //16
+  rowHeight: (1 + index) * 8,
+  scaffoldBlockPxWidth: 8 * index
 };
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      treeData: [
-        {
-          title: "Chicken",
-          expanded: true,
-          children: [{ title: "Egg" }, { title: "Egg" }, { title: "Egg" }]
-        }
-      ]
-    };
-  }
-
-  render() {
-    return (
+function App() {
+  let ref = React.createRef();
+  const [treeData, setTreeData] = useState([
+    {
+      title: "Chicken",
+      expanded: true,
+      children: [{ title: "Egg" }, { title: "Egg" }, { title: "Egg" }]
+    }
+  ]);
+  useEffect(() => {
+    if (ref.current === null || ref.current === 5) {
+      ref.current = 0;
+    }
+    console.log(ref);
+  }, [ref]);
+  return (
+    <AnimatePresence initial={false}>
       <div style={{ height: 500 }}>
         <SortableTree
           {...ReactSortableTreeStyle}
-          treeData={this.state.treeData}
-          onChange={(treeData) => this.setState({ treeData })}
+          treeData={treeData}
+          onChange={(treeData) => setTreeData(treeData)}
+          generateNodeProps={({ node, path }) => {
+            console.log(arguments);
+            ref.current = ref.current + 1;
+            console.log(ref);
+            return {
+              title: (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: ref.current / 4 }}
+                >
+                  <p className="tripleDots">{node.title}</p>
+                </motion.div>
+              )
+            };
+          }}
         />
       </div>
-    );
-  }
+    </AnimatePresence>
+  );
 }
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
